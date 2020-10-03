15c4060ceab4b313e48c1c7902bfd997103c6698
Thomas Ardal <thomasardal@gmail.com>
2020-10-03 20:52:51 +0200

    Moved logging config to Program.cs

--- src/wtf.asp/Program.cs
+++ src/wtf.asp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Elmah.Io.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 
 namespace wtf.asp
@@ -15,6 +16,11 @@ namespace wtf.asp
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
+                .ConfigureLogging(factory =>
+                {
+                    // You will need to replace these parameters with your own available on https://elmah.io
+                    factory.AddElmahIo("168e8ebc7e4d4755bf9f1038ab8b5ecd", Guid.Parse("4edc9492-8c3c-44bf-a727-53f708f2fba5"));
+                })
                 .UseStartup<Startup>()
                 .Build();
 
